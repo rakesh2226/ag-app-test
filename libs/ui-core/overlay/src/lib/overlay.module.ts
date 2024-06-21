b4ffd15a1c5200fa1573efc3b2a2754ecdb01a17
Rakesh <prajapati.rakesh30@gmail.com>
2024-06-21 01:19:35 -0400

    added ui-core theme to ui-core lib

--- libs/ui-core/overlay/src/lib/overlay.module.ts
+++ libs/ui-core/overlay/src/lib/overlay.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PopupOverlayComponent } from './popup-overlay.component';
+import {UiCoreThemeComponent} from '@connect/ui-core-theme';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, UiCoreThemeComponent],
   declarations: [PopupOverlayComponent],
   exports: [PopupOverlayComponent]
 })
